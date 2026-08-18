34524eb115374a76c65db6e8d10cddf9097e4c31
Diego <matos.diegom@gmail.com>
2026-08-18 16:31:55 -0300

    feat: email test

--- src/services/SmtpEmailService.ts
+++ src/services/SmtpEmailService.ts
@@ -2,8 +2,28 @@ import nodemailer from "nodemailer";
 import { EmailService } from "./EmailService";
 
 export class SmtpEmailService implements EmailService {
+
+    private transporter: nodemailer.Transporter;
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: Number(process.env.SMTP_PORT),
+            secure: true,
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASSWORD
+            }
+        });
+    }
+
     async sendEmail(to: string, subject: string, body: string): Promise<void>{
-        
+        await this.transporter.sendMail({
+            from: process.env.SMTP_FROM,
+            to,
+            subject,
+            text: body
+        });
     }
 
 }

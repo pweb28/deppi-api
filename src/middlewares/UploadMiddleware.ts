f07527cdb9bea6e5f33b57da35e33d514161eeb1
Marcos Vinicius <themarcosvn@gmail.com>
2026-08-19 11:18:35 -0300

    upload covers feature (#6)
    * Supabase config file
    
    * cover image upload
    
    * cover uploads
    
    * corrigindo middleware de upload

--- src/middlewares/UploadMiddleware.ts
+++ src/middlewares/UploadMiddleware.ts
@@ -3,19 +3,7 @@ import crypto from "crypto";
 import path from "path";
 
 export const upload = multer({
-  storage: multer.diskStorage({
-    destination(req, file, callback) {
-      callback(null, path.resolve("uploads"));
-    },
-
-    filename(req, file, callback) {
-      const hash = crypto.randomBytes(10).toString("hex");
-
-      const filename = `${hash}-${file.originalname}`;
-
-      callback(null, filename);
-    },
-  }),
+  storage: multer.memoryStorage(),
 
   limits: {
     fileSize: 5 * 1024 * 1024,
